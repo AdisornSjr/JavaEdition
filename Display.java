0df29b202e287b2b13a135caf87619bc6bcfcd84
Adisorn Sancharoen <110969563+AdisornSjr@users.noreply.github.com>
2023-05-14 01:21:03 +0700

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,13 @@
+public class Display { //name of class must be the same as file and Upper at fist letter
+    public static void main(String[] args) {
+        System.out.println("TAN"); //(ln) เหมือนกับ endl
+        System.out.println("TAN");
+        System.out.print("TAN");
+        System.out.print("TAN");
+    }
+}
+
+//setx JAVA_TOOL_OPTIONS "-Dfile.encoding=UTF-8" ตั้งค่าแสดงผลภาษาไทย
+//class first letter must be Uppercase and same as file name
+//Data ทุกตัว lowercase ex. color name age
+//cons ทุกตัว Upper ex. SIZE WIDTH HEIGHT
