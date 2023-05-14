986a2649faa3c393d86fa2ec5e01c18e5a14c7aa
Fluke <adisorn.sjr@gmail.com>
2023-05-14 23:22:50 +0700

    updato

--- Operator.java
+++ Operator.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Operator {
     public static void main(String[] args) {
         // + - * / % หารทำเศษ
+        Scanner sc = new Scanner(System.in);
+        System.out.print("num = ");
+        int a = sc.nextInt();
+        System.out.print("num2 = ");
+        int b = sc.nextInt();
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
     }
 }
