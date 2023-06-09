6fc10dfd08263d4ef28d4016b03345f7c209993b
Fluke <adisorn.sjr@gmail.com>
2023-06-09 15:52:19 +0700

    updato

---
+++ Loop.java
@@ -0,0 +1,7 @@
+public class Loop {
+    public static void main(String[] args) {
+        //while for do..while
+        //ถ้าเจอ break จะออกจาก loop นั้นทันที
+        //continue จะหยุดแล้ววนมาเรื่ม loop ใหม่
+    }
+}
---
+++ Switchcase.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Switchcase {
+    public static void main(String[] args) {
+        //คล้ายๆ if แต่เลือกออกมาทำงานโดยนำค่ามากำหนดเป็นทางเลือกผ่าย case
+        //break จะทำให้ program กระโดดออกไปทำงานนอกคำสั้ง switch ถ้าไม่มีก็จะทำไปเรื่อยๆจนจบ
+        System.out.print("Input number of month = ");
+        Scanner mon = new Scanner(System.in);
+        int num = mon.nextInt();
+        String name;
+        switch(num){
+            case 1: name = "jan";
+                break;
+            case 2: name = "feb";
+                break;
+            default: name = "error";
+        }
+
+        System.out.println(name);
+
+    }   
+
+}
