e592552ff3255966b9128a739aff670567511b04
Fluke <adisorn.sjr@gmail.com>
2023-06-14 00:04:42 +0700

    updato assign

--- Loop.java
+++ Loop.java
@@ -3,5 +3,25 @@ public class Loop {
         //while for do..while
         //ถ้าเจอ break จะออกจาก loop นั้นทันที
         //continue จะหยุดแล้ววนมาเรื่ม loop ใหม่
+        int count = 0;
+        while(true){
+            System.out.println("รอบที่ "+count);
+            count++;
+            if(count == 20){
+                break;
+            }
+        }
+
+        for(int i=0;i<=10;i++){
+            System.out.println("round "+i);
+        }
+
+        do{
+            //condition
+        }while(true);//ถ้าจริงก็กลับไปซ้ำใหม่ ลองทำก่อน 1 รอบ
     }
+
+    //for ใช้ในกรณีรู้รอบชัดเจน
+    //while ไม่รู้จำนวนรอบ
+    //do..while อยากลองทำก่อน
 }
---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int m = 2;
+        for(int i =1;i<=12;i++){
+            int result = m*i;
+            System.out.println(m+"*"+i+" = "+result);
+        }
+    }
+}
